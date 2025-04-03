761d2a671e869e587863fdc04f063e66d6df04d8
Thomas Wessel <turpleturtle12@gmail.com>
2025-04-02 23:00:44 -0700

    Some random changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            O_0
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
@@ -21,10 +21,9 @@ export default async function Home() {
               href="https://create.t3.gg/en/usage/first-steps"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
+              <h3 className="text-2xl font-bold">~!!Test!!~</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+                IDK man
               </div>
             </Link>
             <Link
